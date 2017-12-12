012f013aaf36d0e5f1164b6541405fb8367d87cb
Kathyrivas <kitkatremma@gmail.com>
2017-12-11 16:52:10 -0800

    Add files via upload

---
+++ characters.js
@@ -0,0 +1,28 @@
+
+function changePicture(e){
+	document.getElementById("Goku").classList.toggle("power-up");
+}
+
+function changePicture1(e){
+	document.getElementById("Vegeta").classList.toggle("power-up1");
+}
+
+function changePicture2(e){
+	document.getElementById("Hit").classList.toggle("power-up2");
+}
+
+function changePicture3(e){
+	document.getElementById("Jiren").classList.toggle("power-up3");
+}
+
+function changePicture4(e){
+	document.getElementById("Beerus").classList.toggle("power-up4");
+}
+
+function changePicture5(e){
+	document.getElementById("Zeno").classList.toggle("power-up5");
+}
+
+function play(e){
+	document.getElementById("dbs").play();
+}
